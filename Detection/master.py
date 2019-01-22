ab7e3fb92ee5153cf89ab309111ad82643da1896
QuentinStenhouse <quentinstenhouse@gmail.com>
2019-01-22 12:47:37 +0100

    Add files via upload

---
+++ Detection/master.py
@@ -0,0 +1,47 @@
+import serial
+import time
+import Adafruit_BBIO.GPIO as GPIO
+import sensor
+
+echo = "P9_14" #Sensor echo pin
+trigger = "P9_12" #Sensor trigger pin
+
+if __name__ == '__main__':
+	port = serial.Serial("/dev/ttyO1", baudrate = 9600)
+	SENSOR = sensor.sensor(echo, trigger)
+	SENSOR.GPIO_setup()
+	while True:
+		print("Waiting for the g")
+		receive = port.read().decode()
+		if receive == "G":
+			print(receive)
+			print("Got the ready")
+			port.write(str.encode("F"))
+			SENSOR.get_distance_minus(50)
+			port.write(str.encode("S"))
+			time.sleep(0.1)
+			#print(SENSOR.distan)
+			#Get the UWB position
+			port.write(str.encode("L"))
+			SENSOR.get_distance_plus(75)
+			port.write(str.encode("S"))
+			print(SENSOR.distan)
+			"""port.write(str.encode("F"))
+			SENSOR.get_distance_plus(80)
+			port.write(str.encode("S"))
+			port.write(str.encode("X"))
+		if receive == "D":
+			port.write(str.encode("R"))
+			SENSOR.get_distance_plus(75)
+			port.write(str.encode("S"))
+			port.write(str.encode("F"))
+			port.write(str.encode("S"))
+			port.write(str.encode("Y"))
+		if receive == "M":
+			port.write(str.encode("R"))
+			SENSOR.get_distance_plus(75)
+			port.write(str.encode("S"))
+			#while pos.start() == saved:
+			port.write(str.encode("F"))
+			port.write(str.encode("S"))
+			port.write(str.encode("l"))"""
---
+++ Detection/sensor.py
@@ -0,0 +1,65 @@
+import Adafruit_BBIO.ADC as ADC
+import Adafruit_BBIO.GPIO as GPIO
+from threading import Timer
+from time import sleep
+import time
+
+class sensor:
+
+	#Initialisation of the class sensor
+	def __init__(self,ECHO, TRIGGER):
+		self.echo = ECHO
+		self.trigger =TRIGGER
+
+	def GPIO_setup(self):
+		GPIO.setup(self.trigger, GPIO.OUT)
+		GPIO.setup(self.echo, GPIO.IN)
+
+	#Procedure which will get the distance of an obstacle
+	def get_distance_minus(self, value):
+		pulse_start = 0
+		pulse_end = 0
+		while True:
+			GPIO.output(self.trigger, GPIO.HIGH)
+			sleep(0.00001)
+			GPIO.output(self.trigger, GPIO.LOW)
+			while GPIO.input(self.echo) == 0:
+				pulse_start = time.time()
+			while GPIO.input(self.echo) == 1:
+				pulse_end = time.time()
+			pulse_duration = pulse_end - pulse_start
+			dist = pulse_duration*17150
+			self.distan = round(dist, 2)
+			GPIO.setup(self.echo, GPIO.OUT)
+			GPIO.output(self.echo, GPIO.HIGH)
+			sleep(0.00001)
+			GPIO.setup(self.echo, GPIO.IN)
+			print(dist)
+			if self.distan <= value:
+				return 1
+	def get_distance_plus(self, value):
+                pulse_start = 0
+                pulse_end = 0
+                while True:
+                        GPIO.output(self.trigger, GPIO.HIGH)
+                        sleep(0.0001)
+                        GPIO.output(self.trigger, GPIO.LOW)
+                        while GPIO.input(self.echo) == 0:
+                                pulse_start = time.time()
+                        while GPIO.input(self.echo) == 1:
+                                pulse_end = time.time()
+                        pulse_duration = pulse_end - pulse_start
+                        dist = pulse_duration*17150
+                        self.distan = round(dist, 2)
+                        GPIO.setup(self.echo, GPIO.OUT)
+                        GPIO.output(self.echo, GPIO.HIGH)
+                        sleep(0.00001)
+                        GPIO.setup(self.echo, GPIO.IN)
+                        if self.distan >= value:
+                                return 1
+	#Those procedures will set the parameters of the classe sensor
+	def setEcho(self,Echo):
+		self.echo = Echo
+
+	def setTrigger(self,Trigger):
+		self.trigger = Trigger

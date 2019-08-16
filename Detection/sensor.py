fdbceab133cf29d82e31091bce36ff14dfc9b8b9
QuentinStenhouse <quentinstenhouse@gmail.com>
2019-08-16 21:18:09 +0200

    Update sensor.py

--- Detection/sensor.py
+++ Detection/sensor.py
@@ -1,5 +1,4 @@
-import Adafruit_BBIO.ADC as ADC
-import Adafruit_BBIO.GPIO as GPIO
+import RPi.GPIO as GPIO
 from threading import Timer
 from time import sleep
 import time
@@ -12,32 +11,30 @@ class sensor:
 		self.trigger =TRIGGER
 
 	def GPIO_setup(self):
+		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(self.trigger, GPIO.OUT)
 		GPIO.setup(self.echo, GPIO.IN)
 
-	#Procedure which will get the distance under a value
-	def get_distance_minus(self, value):
+	#Procedure which will get the distance of an obstacle
+	def get_distance(self):
+		GPIO.setup(self.trigger, GPIO.OUT)
+		GPIO.setup(self.echo, GPIO.IN)
 		pulse_start = 0
 		pulse_end = 0
-		while True:
-			GPIO.output(self.trigger, GPIO.HIGH)
-			sleep(0.00001)
-			GPIO.output(self.trigger, GPIO.LOW)
-			while GPIO.input(self.echo) == 0:
-				pulse_start = time.time()
-			while GPIO.input(self.echo) == 1:
-				pulse_end = time.time()
-			pulse_duration = pulse_end - pulse_start
-			dist = pulse_duration*17150
-			self.distan = round(dist, 2)
-			GPIO.setup(self.echo, GPIO.OUT)
-			GPIO.output(self.echo, GPIO.HIGH)
-			sleep(0.00001)
-			GPIO.setup(self.echo, GPIO.IN)
-			print(dist)
-			if self.distan <= value:
-				return 1
-	#Procedure which will get the distance above a value
+		GPIO.output(self.trigger, GPIO.HIGH)
+		sleep(0.00001)
+		GPIO.output(self.trigger, GPIO.LOW)
+		while GPIO.input(self.echo) == 0:
+			pulse_start = time.time()
+		while GPIO.input(self.echo) == 1:
+			pulse_end = time.time()
+		pulse_duration = pulse_end - pulse_start
+		dist = pulse_duration*17150
+		self.distan = round(dist, 2)
+		GPIO.setup(self.echo, GPIO.OUT)
+		GPIO.output(self.echo, GPIO.HIGH)
+		sleep(0.00001)
+		GPIO.setup(self.echo, GPIO.IN)
 	def get_distance_plus(self, value):
                 pulse_start = 0
                 pulse_end = 0
@@ -58,9 +55,10 @@ class sensor:
                         GPIO.setup(self.echo, GPIO.IN)
                         if self.distan >= value:
                                 return 1
-	#Those procedures will set the pins of the beaglebone black for the sensor
+	#Those procedures will set the parameters of the classe sensor
 	def setEcho(self,Echo):
 		self.echo = Echo
 
 	def setTrigger(self,Trigger):
 		self.trigger = Trigger
+
